b60fb0861ce084919585ecfcbca90571ef8e4eb7
lindaaatvare <endijsvilde@gmail.com>
2022-01-17 15:03:56 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Liene Ozoliņa','Signe Piepe','Jānis Bērziņš','Mārtiņš Beka','Endijs Smilga'];
+const balva=['dators','telefons','mašīna','veļasmašīna','trauku mašīna'];
+const naudaKopa=1000000;
+let uzvaretajuSkaits=5;
+let rand=Math.random()*10;
+rand=Math.floor(rand);//noapalo skaitli uz leju
+console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
