532100b1a4e4641b44fed71693b5929ea5941633
lindaaatvare <endijsvilde@gmail.com>
2022-01-24 14:51:07 +0200

    viens

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const vardi=['Liene Ozoliņa','Signe Piepe','Jānis Bērziņš','Mārtiņš Beka
 const balva=['dators','telefons','mašīna','veļasmašīna','trauku mašīna'];
 const naudaKopa=1000000;
 let uzvaretajuSkaits=5;
-for(let i=0;i<5;i++);{
+for(let i=0;i<5;i++){
 let rand=Math.random()*vardi.length;
 rand=Math.floor(rand);//noapalo skaitli uz leju
 console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
